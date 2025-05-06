62ff7cd5572afbc051cc7262d0cf1b4b399433ca
ngoctan <ngoctan300904@gmail.com>
2025-05-06 21:22:58 +0700

    first commit

---
+++ store_tiki_products.py
@@ -0,0 +1,47 @@
+import psycopg2
+from config import load_config
+import os
+import json
+
+def insert_data():
+    config = load_config()
+    sql = """INSERT INTO products (id, name, url_key, price, description, images)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+
+                # List all file in output folder
+                for file in os.listdir('output'):
+                    # check if the file is json type or not
+                    if file.endswith('.json'):
+                        path = os.path.join('output', file)
+                        with open (path, "r", encoding='utf-8') as readfile:
+                            products = json.load(readfile)
+                            for product in products:
+                                cur.execute(sql, (product.get("id"),
+                                        product.get("name"),
+                                        product.get("url_key"),
+                                        product.get("price"),
+                                        product.get("description"),
+                                        product.get("images")))
+                            conn.commit()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+if __name__ == "__main__":
+    insert_data() # call insert function to insert all data into the table in DTB
+
+    # check the number of data in the table: expected output: 198942
+    config = load_config()
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                                select* from products;
+                            """)
+                rows = cur.rowcount
+                print(rows)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
